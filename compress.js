142891e9b2c34790b2f84c7b54af3d8dfb7a835e
Andrew S <andrews54757@gmail.com>
2017-06-12 17:53:17 -0400

    Create compress.js

--- compress.js
+++ compress.js
@@ -4,12 +4,17 @@ var img = new Image();
 img.src = "/andrew.png";
 
 img.onload = function() {
+     var canvas = document.createElement('canvas'); // create canvas
+            var ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth; // fill screen
+  canvas.height = window.innerHeight;
+  
+  document.body.appendChild(canvas); // append to site
+  
+  ctx.drawImage(img, 0, 0 );
+  var imageData = ctx.getImageData(0, 0, img.width, img.height);
   
   
-  
-  
-  
-  
-  
+  console.log(imageData)
   
 }
